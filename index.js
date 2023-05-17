fdd67bba20e3763a0ffc87d02e1af2181ced142b
sarasg89 <118869836+sarasg89@users.noreply.github.com>
2023-05-16 21:38:29 -0400

    added cors support

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 app.use(express.json());
 app.use(express.static('public'));
+
+var corsOptions = {
+    origin: 'https://sarasg89.github.io/',
+    optionsSuccessStatus: 200 // For legacy browser support
+}
+
+app.use(cors(corsOptions));
 
 const emailjs = require('@emailjs/nodejs');
 require('dotenv').config({ path:__dirname+'/.env' });
